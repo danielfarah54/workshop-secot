3d350a404db5156844130cabb5989569b7595a6f
Daniel Farah <danielfarah54@gmail.com>
2021-08-23 22:18:17 -0300

    update code style

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 const btnMenu = document.getElementById("btn-menu");
 
 function toggleMenu() {
-    const nav = document.getElementById("nav");
-    nav.classList.toggle("active");
+  const nav = document.getElementById("nav");
+  nav.classList.toggle("active");
 }
 
 btnMenu.addEventListener("click", toggleMenu);
